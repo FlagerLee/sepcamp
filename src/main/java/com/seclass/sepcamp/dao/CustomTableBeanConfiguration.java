11d224daea7d46ac6dbec231c1fd873544dfb608
FlagerLee <yc-li18@mails.tsinghua.edu.cn>
2022-10-30 17:52:15 +0800

    实现启动时创建数据库

--- src/main/java/com/seclass/sepcamp/dao/CustomTableBeanConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.seclass.sepcamp.dao;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableConfigurationProperties(CustomTableConfiguration.class)
-public class CustomTableBeanConfiguration {
-}
