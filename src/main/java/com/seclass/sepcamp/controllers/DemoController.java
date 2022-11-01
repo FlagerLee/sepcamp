e3852a65458cc198c8b608c2ee90aea66ffa3c5a
dingyi <1220614922@qq>
2022-11-01 12:37:04 +0800

    sql修改

--- src/main/java/com/seclass/sepcamp/controllers/DemoController.java
+++ src/main/java/com/seclass/sepcamp/controllers/DemoController.java
@@ -1,4 +1,4 @@
-package com.seclass.sepcamp.controller;
+package com.seclass.sepcamp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
