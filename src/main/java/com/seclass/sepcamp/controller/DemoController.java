72b25f71a5a0b597ccd61d0381042d38276ad71e
FlagerLee <46860743+FlagerLee@users.noreply.github.com>
2022-12-09 16:39:51 +0800

    Revert "Feature djx form team"

--- src/main/java/com/seclass/sepcamp/controller/DemoController.java
+++ src/main/java/com/seclass/sepcamp/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.seclass.sepcamp.controllers;
+package com.seclass.sepcamp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
